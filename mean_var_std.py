a6f2b3b8d2f5555b0d7ed9213a64e499b8c59b02
Facundo Fernández <ffernanndez97@gmail.com>
2024-10-02 10:46:39 -0300

    Update mean_var_std.py
    Developing mean rows and columns

--- mean_var_std.py
+++ mean_var_std.py
@@ -4,6 +4,16 @@ def calculate(list):
 
     ls= np.array(list)
 
+    mean1 = [ls[[0, 1, 2]].mean(), ls[[3, 4, 5]].mean(), ls[[6, 7, 8]].mean()]
+    mean2 = []
 
+    return {
 
-    return calculations
+        'mean': [axis1, axis2, flattened],
+        'variance': [axis1, axis2, flattened],
+        'standard deviation': [axis1, axis2, flattened],
+        'max': [axis1, axis2, flattened],
+        'min': [axis1, axis2, flattened],
+        'sum': [axis1, axis2, flattened]
+
+    }
